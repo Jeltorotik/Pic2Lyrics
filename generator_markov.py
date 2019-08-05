dee0f522907c8a9ffc25ca63cf395a7ca22a9e17
Jeltorotik <38293253+Jeltorotik@users.noreply.github.com>
2019-08-05 14:32:37 +0300

    Update generator_markov.py

--- generator_markov.py
+++ generator_markov.py
@@ -1,15 +1,22 @@
 import markovify
 
-with open("poems.txt", 'r', encoding = 'utf-8', errors='ignore') as f:
-    text = f.read()
-    f.close()
+markov_params = open('model_params/markov_params.json').read()
+rev_markov_params = open('model_params/rev_markov_params.json').read()
 
-text_model = markovify.Text(text)
+model = markovify.Text.from_json(markov_params)
+rev_model = markovify.Text.from_json(rev_markov_params)
 
+#Generator
 def generate(start):
     try:
-        return text_model.make_sentence_with_start(start)
+        prefix = rev_model.make_sentence_with_start(start, tries = 10)
+        postfix = model.make_sentence_with_start(start, tries = 10)
+        prefix = ' '.join(prefix.split()[::-1][1:-1]) + ' '
+        out = prefix + postfix
+        for i in range(1, len(out)):
+            if out[i].isupper():
+                out = out[:i-1] + '\n' + out[i:]
     except:
-        return None
-
+        out = None
+    return out
 
